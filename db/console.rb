0631fe24517fdf818f78d80f74bb5f2efb1dd61e
Mike-Heneghan <m.heneghan@live.co.uk>
2018-03-21 18:17:19 +0000

    Return all album object and delete all albums functions added to the album class.

--- db/console.rb
+++ db/console.rb
@@ -4,12 +4,19 @@ require("pg")
 require_relative("../models/album")
 require_relative("../models/artist")
 
-artist1 = Artist.new('name' => 'Led Zeppelin')
+artist1 = Artist.new('name' => 'Arcade Fire')
 
-artist1.save()
-artist1.save()
-artist1.save()
-artist1.save()
+# Artist.delete_all
+#
+# artist1.save()
+
 # artist1.delete()
 # Artist.delete_all()
 p Artist.all()
+puts ""
+
+p album1 = Album.new('title' => 'The Suburbs', 'genre' => "Indie Rock", 'artist_id' => 1 )
+
+# album1.save()
+# album1.delete()
+Album.delete_all
--- models/album.rb
+++ models/album.rb
@@ -1,19 +1,52 @@
 class Album
-  attr_reader :id, :title, 
+  attr_reader :id, :title, :genre, :artist_id
 
   def initialize(options)
-    @id = options['id']
-    @name = options['name']
+    @id = options['id'].to_i
+    @title = options['title']
+    @genre = options['genre']
+    @artist_id = options['artist_id'].to_i
   end
 
   def save()
 
     sql = "
     INSERT INTO albums
+    (title,genre,artist_id)
+    VALUES ($1,$2,$3)
+    ;
+    "
+    values = [@title, @genre, @artist_id]
 
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+
+    sql = "
+    DELETE FROM albums
     ;
     "
 
+    SqlRunner.run(sql)
   end
 
+  def self.all()
+
+    sql = "
+    SELECT * FROM albums
+    ;
+    "
+    result = SqlRunner.run(sql)
+    album_objects = result.map { |album_hash| Album.new(album_hash)  }
+    return album_objects
+  end
+
+  def self.delete_all()
+
+    sql = "DELETE FROM albums;"
+
+    SqlRunner.run(sql)
+
+  end
 end
