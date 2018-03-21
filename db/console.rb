ec479fb01aa06e0b4c946ac33d270d88d7e46a02
Mike-Heneghan <m.heneghan@live.co.uk>
2018-03-21 22:19:49 +0000

    Newed Up more artists and albums to further test correct methods.

--- db/console.rb
+++ db/console.rb
@@ -4,23 +4,39 @@ require("pg")
 require_relative("../models/album")
 require_relative("../models/artist")
 
-artist1 = Artist.new('name' => 'Arcade Fire')
+
 
 Album.delete_all
 Artist.delete_all
 
-#
-artist1.save()
+artist1 = Artist.new('name' => 'Arcade Fire')
+artist2 = Artist.new('name' => 'The XX')
 
+artist1.save()
+artist2.save()
 # artist1.delete()
 # Artist.delete_all()
 p Artist.all()
 puts ""
 
 album1 = Album.new({'title' => 'The Suburbs', 'genre' => "Indie Rock", 'artist_id' => artist1.id })
+album2 = Album.new({'title' => 'The Suburbs', 'genre' => "Indie Rock", 'artist_id' => artist1.id })
+album3 = Album.new({'title' => 'Everything Now', 'genre' => "Indie Rock", 'artist_id' => artist1.id })
+album4 = Album.new({'title' => 'Reflektor', 'genre' => "Indie Rock", 'artist_id' => artist1.id })
+
+album5 = Album.new({'title' => 'xx', 'genre' => "Indie Electronic", 'artist_id' => artist2.id })
+album6 = Album.new({'title' => 'Coexist', 'genre' => "Indie Electronic", 'artist_id' => artist2.id })
+album7 = Album.new({'title' => 'I See You', 'genre' => "Indie Electronic", 'artist_id' => artist2.id })
+
 
 
 album1.save()
+album2.save()
+album3.save()
+album4.save()
+album5.save()
+album6.save()
+album7.save()
 
 p Album.all()
 puts ""
@@ -43,8 +59,8 @@ artist1.edit()
 p Artist.all()
 puts ""
 
-p Artist.find_by_id(26)
+p Artist.find_by_id(7)
 puts ""
 
-p Album.find_by_id(26)
+p Album.find_by_id(9)
 puts ""
