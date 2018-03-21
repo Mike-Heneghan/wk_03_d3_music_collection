dd36c87b1a836fc8db505cd330c4214a82235c7e
Mike-Heneghan <m.heneghan@live.co.uk>
2018-03-21 19:37:39 +0000

    album artist_id bug fixed. MVP completed successfully.

--- db/console.rb
+++ db/console.rb
@@ -6,17 +6,29 @@ require_relative("../models/artist")
 
 artist1 = Artist.new('name' => 'Arcade Fire')
 
-# Artist.delete_all
+Album.delete_all
+Artist.delete_all
+
 #
-# artist1.save()
+artist1.save()
 
 # artist1.delete()
 # Artist.delete_all()
 p Artist.all()
 puts ""
 
-p album1 = Album.new('title' => 'The Suburbs', 'genre' => "Indie Rock", 'artist_id' => 1 )
+album1 = Album.new({'title' => 'The Suburbs', 'genre' => "Indie Rock", 'artist_id' => artist1.id })
 
-# album1.save()
+
+album1.save()
+
+p Album.all()
+puts ""
+
 # album1.delete()
-Album.delete_all
+# Album.delete_all
+p artist1.albums_by_artist()
+puts ""
+
+p album1.artist_of_album()
+puts ""
